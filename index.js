341a04ab38c63910f44daa8121c77686313c8079
patarapornkan <patt.anantarangsi@gmail.com>
2021-10-13 12:42:32 +0700

    update game logic

--- index.js
+++ index.js
@@ -16,12 +16,9 @@ let gridArray = [
 
 let warderTurn = true;
 
-let socketIds = [];
-
 let allClientIds = [];
 
 let players = [];
-
 
 
 io.on('connection', socket => {
@@ -34,7 +31,8 @@ io.on('connection', socket => {
                 id: socket.id,
                 role: '',
                 pos_x: 0,
-                pos_y: 0
+                pos_y: 0,
+                score: 0
             }
         players.push(player);
         console.log(players);
@@ -59,11 +57,8 @@ io.on('connection', socket => {
       }
    });
 
-    socket.on('move', (ret)=> {
-        console.log(socket.id);
-        console.log(ret);
-
-       
+    socket.on('move', (direction)=> {
+        move(direction, socket.id, socket);
     })
 })
 
@@ -104,12 +99,106 @@ function setRoles(){
     }
 }
 
+function setPositions(){
+    for(let i=0; i<5; i++){
+        for(let j=0; j<5; j++){
+            if (gridArray[i][j]===3){
+                players.find(player=> player.role==='warder').pos_x = i;
+                players.find(player=> player.role==='warder').pos_y = j;
+            }
+            if (gridArray[i][j]===4){
+                players.find(player=> player.role==='prisoner').pos_x = i;
+                players.find(player=> player.role==='prisoner').pos_y = j;
+            }
+        }
+    }
+}
+
 function startGame(){
     const newGrid = randomGrid(gridArray);
     gridArray = newGrid;
     setRoles();
+    setPositions();
+    // console.log(players);
     warderTurn = true;
     io.emit('initializeRole', players); 
     io.emit('newGrid', newGrid);
 }
 
+function move(direction, id, socket){
+    const currentPlayer = players.find(player => player.id === id);
+    if (!((currentPlayer.role==='warder'&& warderTurn)||(currentPlayer.role==='prisoner'&&!warderTurn))){
+        return;
+    }
+    console.log(currentPlayer);
+    let new_x = currentPlayer.pos_x;
+    let new_y = currentPlayer.pos_y;
+    console.log(`current pos_x:${new_x}`);
+    console.log(`current pos_y:${new_y}`);
+    switch(direction){
+        case 'left':
+            new_y = new_y-1;
+            break;
+        case 'right':
+            new_y = new_y+1;
+            break;
+        case 'up':
+            new_x = new_x-1;
+            break;
+        case 'down':
+            new_x = new_x+1;
+            break;
+        default:
+            return;
+    }
+    //check if new pos is within the grid  
+    if(new_x>=0 && new_x<=4 && new_y>=0 && new_y<=4){
+        if(currentPlayer.role === 'warder' && warderTurn){
+            if (gridArray[new_x][new_y] === 4){  
+                warderWins();
+            } else if(gridArray[new_x][new_y]===0){ //if new position is a freeblock
+                gridArray[currentPlayer.pos_x][currentPlayer.pos_y]=0;
+                gridArray[new_x][new_y] = 3;
+                players.find(player => player.id === id).pos_x = new_x;
+                players.find(player => player.id === id).pos_y = new_y;
+                io.sockets.emit("newGrid", gridArray);
+                toggleTurn();
+            }
+        } else if(currentPlayer.role === 'prisoner' && !warderTurn){
+            if (gridArray[new_x][new_y] === 2){  
+                prisonerWins();
+            } else if (gridArray[new_x][new_y] === 3){
+                warderWins();
+            } else if (gridArray[new_x][new_y] === 0){
+                gridArray[currentPlayer.pos_x][currentPlayer.pos_y]=0;
+                gridArray[new_x][new_y] = 4;
+                players.find(player => player.id === id).pos_x = new_x;
+                players.find(player => player.id === id).pos_y = new_y;
+                io.sockets.emit("newGrid", gridArray);
+                // socket.emit("newGrid", gridArray);
+                toggleTurn();
+            }
+        } else{
+            console.log('invalid role');
+            return;
+        }
+    }
+}
+
+function warderWins(){
+    //warder score + 1
+    console.log('warderWins')
+}
+
+function prisonerWins(){
+    //warder score -1 
+    console.log('prisoner wins')
+}
+
+function toggleTurn(){
+    warderTurn = !warderTurn;
+    console.log('toggleTurn');
+    console.log(`warderTurn: ${warderTurn}`);
+    //notify both clients that the new round starts. will have to send warderTurn
+    console.log('restartTimer');
+}
